8f7df7550a99956eac388272d4b797f4bb593fd7
Jinwei Zhao <i@ring0.xyz>
2016-05-22 18:31:41 +0800

    remove about & update

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,17 +3,22 @@ var sleep = function(ms, callback) {
             callback()
         }, ms)
     },
-    favthings = ["raspberrypi", "Linux", "Let's Encrypt", "Ingress"],
+    favthings = ["raspberrypi", "Linux", "Let's Encrypt", "Ingress", "Computer Vision", 
+"Machine Learning"],
     removeClass = function(el, className) {
-        el.classList ? el.classList.remove(className) : el.className = el.className.replace(new RegExp("(^|\\b)" + className.split(" ").join("|") + "(\\b|$)", "gi"), " ")
+        el.classList ? el.classList.remove(className) : el.className = 
+el.className.replace(new RegExp("(^|\\b)" + className.split(" ").join("|") + "(\\b|$)", 
+"gi"), " ")
     },
     addClass = function(el, className) {
         el.classList ? el.classList.add(className) : el.className += " " + className
     },
     i = 0,
     nextThing = function(thing) {
-        i < favthings.length - 1 ? i++ : i = 0, removeClass(thing, "slideInDown"), addClass(thing, "slideOutUp"), sleep(700, function() {
-            thing.innerHTML = favthings[i], removeClass(thing, "slideOutUp"), addClass(thing, "slideInDown")
+        i < favthings.length - 1 ? i++ : i = 0, removeClass(thing, "slideInDown"), 
+addClass(thing, "slideOutUp"), sleep(700, function() {
+            thing.innerHTML = favthings[i], removeClass(thing, "slideOutUp"), 
+addClass(thing, "slideInDown")
         })
     };
 document.addEventListener("DOMContentLoaded", function() {
@@ -24,3 +29,4 @@ document.addEventListener("DOMContentLoaded", function() {
         nextThing(thing)
     }, 4e3)
 });
+
