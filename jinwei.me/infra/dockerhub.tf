1863462e23f8d7099bb26c3a11d5ed9a2cce54cd
clarkzjw <i@jinwei.me>
2023-01-11 19:55:59 -0800

    use aws_ssm

--- jinwei.me/infra/dockerhub.tf
+++ jinwei.me/infra/dockerhub.tf
@@ -10,7 +10,7 @@ resource "aws_ssm_parameter" "dockerhub_token" {
   value = var.dockerhub_token
 }
 
-resource "aws_secretsmanager_secret" "datadog_api_key" {
+resource "aws_ssm_parameter" "datadog_api_key" {
   name = "/${local.name}/datadog/api_key"
   type = "SecureString"
   value = var.datadog_api_key
